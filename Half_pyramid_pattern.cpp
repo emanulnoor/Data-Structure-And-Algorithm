f812ece3adb90db2e6843282c36f639b474cb5e1
Noor Ul Eman <eemanulnoor@gmail.com>
2024-12-30 23:55:59 +0500

    Half_pyramid_pattern.cpp

---
+++ Half_pyramid_pattern.cpp
@@ -0,0 +1,16 @@
+        //program for the half pyramid
+#include<iostream> 
+using namespace std; 
+int main(){
+        
+         int n=6;
+         int i,j;
+         for(i=1;i<=n;i++){
+            for(j=1;j<=i;j++){
+                cout<<j<<"\t";
+            }
+            cout<<endl;
+         }
+     return 0;
+
+}
