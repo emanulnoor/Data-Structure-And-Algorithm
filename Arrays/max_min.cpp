c867dc76ff3e366c901adf32a39c0d957500200f
emanulnoor <eemanulnoor@gmail.com>
2025-01-16 22:10:33 +0500

    new file:   Arrays/max_min.cpp

---
+++ Arrays/max_min.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+    int arr[5]={8,9,4,1,10};
+    int n=sizeof(arr)/sizeof(int);
+    //for max
+    int max=arr[0];
+    for(int i=0;i<n;i++){
+        if(arr[i]>max){
+            max=arr[i];
+        }
+    }
+    cout<<"THE maximum value in array is "<<max<<endl;
+
+    //for min
+    int min=arr[0];
+    for(int i=0;i<n;i++){
+        if(arr[i]<min){
+            min=arr[i];
+        }
+    }
+    cout<<"THE minimum value in array is "<<min<<endl;
+
+    return 0;
+}
