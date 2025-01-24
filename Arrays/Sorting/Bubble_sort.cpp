9da78b0469d222dbf3c78f5a7681eda329e9afe2
emanulnoor <eemanulnoor@gmail.com>
2025-01-24 22:20:22 +0500

    Bubble_sort

--- Arrays/Sorting/Bubble_sort.cpp
+++ Arrays/Sorting/Bubble_sort.cpp
@@ -3,11 +3,12 @@ using namespace std;
 int bublesorting(int arr[],int n){
     for(int i=0;i<n-1;i++){
         for(int j=0;j<=n-i-1;j++){
-            int temp;
+            // int temp;
             if(arr[j]>arr[j+1]){
-                temp=arr[j+1];
-                arr[j+1]=arr[j];
-                arr[j]=temp;
+                swap(arr[j],arr[j+1]);
+                // temp=arr[j+1];
+                // arr[j+1]=arr[j];
+                // arr[j]=temp;
                 
             }
         }   
