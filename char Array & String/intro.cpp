1c2f7e8cb8efd5a7b010f255caca311839b0e758
emanulnoor <eemanulnoor@gmail.com>
2025-02-03 23:15:36 +0500

    intro

--- char Array & String/intro.cpp
+++ char Array & String/intro.cpp
@@ -7,7 +7,11 @@ string str1="DSA ";
 string str2="In C++";
 //concatenation
 string str3=str1+str2;
-cout<<str3<<endl;
+// cout<<str3<<endl;
+//printing using each loop
+for(char ch: str3){
+    cout<<ch;
+}
 //for checking if str1 = or != to str2
 cout<<(str1==str2)<<endl; //output false ..0
 //for the length
